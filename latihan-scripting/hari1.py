ac4fca53b4c13bd58cfadc4aae41c42958c1bae5
anonim301 <mufidyanuar@gmail.com>
2026-01-12 01:58:54 +0700

    Create hari1.py
    Hari pertama belajar python.Ini merupakan dokumentasi belajar python

---
+++ latihan-scripting/hari1.py
@@ -0,0 +1,17 @@
+name = input("Siapa namamu: ")
+kelas = int(input("kamu kelas berapa: "))
+sekolah = input("Dimana sekolahmu: ")
+hobi = input("Hobimu: ")
+uang = float(input("Berapa uang jajanmu"))
+
+uang_seminggu = (uang * 6)
+
+#teks
+print("KARTU NAMA DIGITAL")
+print("# * 10")
+
+print(f"name      : {name}")
+print(f"kelas      : {kelas}")
+print(f"sekolah  : {sekolah}")
+print(f"Hobi        : {hobi}")
+print(f"Uang jajanmu seminggu : {uang_seminggu}  ")
