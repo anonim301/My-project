55465c469f80189d255bb0f6e89bd464113ba2b7
anonim301 <mufidyanuar@gmail.com>
2026-01-19 11:26:43 +0700

    Create day2.py

---
+++ belajar_python/day2.py
@@ -0,0 +1,9 @@
+print("BOT TEMAN NGOBROL")
+name = input("Siapa namamu? ")
+print(f"Iya salam kenal {name}! ")
+
+umur = int(input("Berapa umurmu? "))
+print(f"Wah ternyata umur kita sama")
+
+hobi = input("Apa hobimu? ")
+print(f"Saya juga suka hobi {hobi}")
