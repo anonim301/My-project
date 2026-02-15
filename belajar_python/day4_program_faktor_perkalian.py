87a1775ecaae516f2ac3400ae1ba8d69d3407753
anonim301 <mufidyanuar@gmail.com>
2026-02-15 21:29:32 +0700

    Create day4_program_faktor_perkalian.py

---
+++ belajar_python/day4_program_faktor_perkalian.py
@@ -0,0 +1,9 @@
+angka = int(input("Masukkan angka dari (1-10): "))
+
+print("====PROGRAM TABEL PERKALIAN====")
+
+for i in range(1, 21):
+        print(f"{angka} x {i} = {angka * i}")
+
+
+                                                                
